3c2250baff0496e36a8f65752baac26af696dd6c
Theodore J. Roggeman <troggeman@gatetm.tk>
2020-12-15 13:34:45 +0100

    added file sizes to the output

--- stamped.pl
+++ stamped.pl
@@ -1,22 +1,40 @@
 #!/usr/bin/perl
-# $Id: stamped.txt,v 1.0.1 2020-6-30 10:02:24 michelc Exp $
+# $Id: stamped.pl,v 1.9.7 2020-12-15 13:32:17 michelc Exp $
 #
 # $birth: 1593504220$
 # This script replace "added" with timestamp in IPFS log.
+#  and add the file size before its name
 #
 # usage:
 #   ipfs add -r . | perl -S stamped.pl | tee -a added.log
 #
-# - 4354b @(#) GQU : 30-Jun-20 (MGCM) 10:02:53 - WW26 [b]
+# - 8389b @(#) GQU : 15-Dec-20 (MGC.) 13:32:56 - WW50 [b]
 #
+my $ptime = time;
 while (<>) {
   if (/added\s(\w+)\s*(.*)/) {
     my $f = $2;
     chomp($f);
-    $f = '../'.$f if (! -e $f && -e "../$f");
-    if (-e $f) {
-     my ($size,$atime,$mtime,$ctime) = (lstat($f))[7,8,9,10];
-     s/added/$mtime/;
+    if ("$f" eq '') {
+       s/added/$ptime/;
+    } else {
+       if (! -e $f && -e "../$f") {
+         s,$f,../$f,;
+         $f = '../'.$f;
+       }
+       if (-d $f && $f !~ m,/$,) {
+         s,$f,$f/,;
+         $f = $f.'/';
+       }
+       if (-e $f) {
+          my ($size,$atime,$mtime,$ctime) = (lstat($f))[7,8,9,10];
+          s/added/$mtime/;
+          s,$f,$size $f,;
+          $ptime = $mtime;
+       } else {
+          my $tics = time;
+          s/added/$tics/;
+       }
     }
   }
   print;
