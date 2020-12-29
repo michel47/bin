5409702272df5f755a39bf9863333fb888a9fb5a
Rebekah C. Elquist <relquist@bigtm.gq>
2020-12-29 07:20:50 +0100

    pushed a few scripts !

--- getyt.sh
+++ getyt.sh
@@ -1,2 +1,11 @@
 # 
-youtube-dl -r 50K -f 'best[height <=? 360]' -k "$(clip -o)" "$1"
+
+if [ "x$1" == 'x' ]; then
+url="$(xclip -o)"
+echo "url: $url"
+else
+url="$1"
+echo "url: $url"
+shift
+fi
+youtube-dl -r 50K -f 'best[height <=? 360]' -k "$url" $*
