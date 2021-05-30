db70ab8c82117dd5c6b3c2d4be72ae0e8eee0c75
Aaron I. Kaewprasert <akaewprasert@kintm.gq>
2021-05-30 10:20:32 +0200

    changed && s=1 to || s=0

--- sunping.sh
+++ sunping.sh
@@ -1,24 +1,58 @@
 #
 
+s=1
+ipfs=/home/michelc/.../ipfs/bin/ipfs
 tic=$(date +%s%N | cut -c-13)
 cachedir=$HOME/.cache/logs
 if [ ! -d $cachedir ]; then mkdir $cachedir; fi
 echo $tic: $(date)
-ping -D -A -W 3 -c 23 sunrise.com | tee -a $cachedir/sunping.log 2>&1 
-echo -n "status: $?" >> $cachedir/sunping.log
-ping -D -A -W 3 -c 19 www.linkedin.com
-echo -n " $?" >> $cachedir/sunping.log
-ping -D -A -W 3 -c 17 gist.github.com
-echo -n " $?" >> $cachedir/sunping.log
-ping -D -A -W 3 -c 13 google.com
-echo -n " $?" >> $cachedir/sunping.log
-ping -D -A -W 3 -c 11 gateway.ipfs.io
-echo -n " $?" >> $cachedir/sunping.log
-ping -D -A -W 3 -c 7 opentimestamps.org
-echo  " $?." >> $cachedir/sunping.log
-echo .
+echo "my internet is down" | $ipfs add --raw-leaves -pin=true --progress=false --cid-base base58btc
+#$ipfs dht provide QmVS6qA5AYu5d5yVc1uKALJbSGcHZtqUCLMy4UeUR5mU4p &
+$ipfs dht provide zb2rhaAgAM19HByZSiSENFsG5Bz9bAScvXZmobJ5XnJieoshc &
+#exit;
 
-#env DISPLAY=:0 firefox https://gist.github.com
+ping -D -A -W 3 -c 23 sunrise.com >> $cachedir/sunping.log || s=0
+echo -n "$tic: status=$?" >> $cachedir/sunstatus.log
+ping -D -A -W 3 -c 19 www.linkedin.com || s=0
+echo -n " $?" >> $cachedir/sunstatus.log
+ping -D -A -W 3 -c 17 gist.github.com || s=0
+echo -n " $?" >> $cachedir/sunstatus.log
+ping -D -A -W 3 -c 13 google.com || s=0
+echo -n " $?" >> $cachedir/sunstatus.log
+ping -D -A -W 3 -c 11 gateway.ipfs.io || s=0
+echo -n " $?" >> $cachedir/sunstatus.log
+ping -D -A -W 3 -c 7 opentimestamps.org || s=0
+echo -n " $?" >> $cachedir/sunstatus.log
+ping -D -A -W 3 -c 5 opensea.io && s=1
+echo  " $?." >> $cachedir/sunstatus.log
+p=$(cat $cachedir/sunstatus.txt)
+echo $s > $cachedir/sunstatus.txt
+echo "$p->$s."
+
+if [ "$s" = '0' -a "$p" != '0' ]; then
+echo "My Internet down again" > /home/michelc/.ipfs/blocks/ED/AFKREIBULR2LB6AUY75KCVPVL6L7EWGWQ2YBY4EE4AOXTHNZ5VSGYFYEDM.data
+qm=$($ipfs add -w --pin=false $cachedir/sun*.log -Q)
+#env DISPLAY=:0 firefox http://localhost:8390/ipfs/$qm/sunstatus.log
+ping -c 1 gateway.ipfs.io 1>/dev/null
+ping -c 1 wepik.com 1>/dev/null
+ping -c 1 nft.storage 1>/dev/null
+ping -c 1 na-west1.surge.sh 1>/dev/null
+ping -c 1 michelc.netlify.com 1>/dev/null
+ping -c 1 freepikcompany.com 1>/dev/null
+# echo "Your Internet is DOWN" | $ipfs add
+env DISPLAY=:0 firefox http://localhost:8390/ipfs/Qmaq9GJrmJdMPxo9ThC8NRk6zjQkPUyZKcNC1uYhBHDPVb
+
+#env DISPLAY=:0 firefox http://localhost:8390/ipfs/zb2rhaAgAM19HByZSiSENFsG5Bz9bAScvXZmobJ5XnJieoshc
+fi
+if [ "$s" = '1' -a "$p" = '0' ]; then
+$ipfs dht findprovs -n 1 zb2rhaAgAM19HByZSiSENFsG5Bz9bAScvXZmobJ5XnJieoshc
+echo "My internet is UP!" > /home/michelc/.ipfs/blocks/ED/AFKREIBULR2LB6AUY75KCVPVL6L7EWGWQ2YBY4EE4AOXTHNZ5VSGYFYEDM.data
+curl -4 https://icanhazip.com
+echo "Your internet connection is back UP" | $ipfs add
+#env DISPLAY=:0 firefox http://gateway.ipfs.io/ipfs/QmbbMRJJyuNdPniaCmgqFiNR5CBm17Kd8AMsC1SFzUwTVr
+env DISPLAY=:0 firefox http://localhost:8390/ipfs/zb2rhaAgAM19HByZSiSENFsG5Bz9bAScvXZmobJ5XnJieoshc
+#env DISPLAY=:0 firefox https://ipfs.blockringtm.ml/ipfs/zb2rhaAgAM19HByZSiSENFsG5Bz9bAScvXZmobJ5XnJieoshc
+fi
 
 exit $?
 true;
